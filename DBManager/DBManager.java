ecba021b0895cedb109a83cf75c1b10e9f17ad99
Yuxiang Wang <ethanyuwang@gmail.com>
2016-11-21 21:42:50 -0800

    Updated Database

--- DBManager/DBManager.java
+++ DBManager/DBManager.java
@@ -1,16 +1,11 @@
 import java.sql.*;
 
-/* To-do:
- * too many "NOT NULL"
- * e.g. Users topic word, screenname can be NULL?
- */
-
 public class DBManager {
 
         public static void main(String[] args){
 
                 try{
-			// 1. Get connection to database
+			// Get connection to database
                         Class.forName("oracle.jdbc.driver.OracleDriver");
                         String url = "jdbc:oracle:thin:@uml.cs.ucsb.edu:1521:xe";
 			String username = "glee";
@@ -25,18 +20,229 @@ public class DBManager {
 			//deleteTables(con);
 
 			//Print Table
-			printTable(con, "USERS");
+			//printTable(con, "USERS");
 
-			// 4. Process the result set
-                        //while(rs.next())
-				//MODIFY PRINT TO FIT YOUR QUERY AND ATTRIBUTE TYPES
-                        //        System.out.println(rs.getInt(1)+" "+rs.getString(2));
+			//Print GLEE Tables
+			//printGLEETables(con);
 
-			// 5 Close conenction
+			//Print all tables
+			//printAllTables(con);
+
+			// Close conenction
                         con.close();
                 }
                 catch(Exception e){System.out.println(e);}
         }
+
+	public static void createTables(Connection con){
+		try{
+			// Create tables without foreign keys
+			Statement st = con.createStatement();
+			
+			String sql = "CREATE TABLE Users " +
+	  		"(email_address VARCHAR(20) NOT NULL, " +
+	  		" password VARCHAR(10) NOT NULL, " +
+	  		" name VARCHAR(20) NOT NULL, " +
+	  		" phone_number NUMERIC(10) NOT NULL, " +
+	  		" screenname VARCHAR(20), " +
+	  		" PRIMARY KEY (email_address))";
+			st.executeQuery(sql);	
+		
+			sql = "CREATE TABLE Managers " +
+	  		"(email_address VARCHAR(20) NOT NULL)";
+			st.executeQuery(sql);
+
+			sql = "CREATE TABLE Circle_feeds " +
+	  		"(circle_id INT NOT NULL, " +
+	  		" owner VARCHAR(20) NOT NULL, " +
+	  		" PRIMARY KEY (Circle_id), " +
+	  		" UNIQUE (owner))";
+			st.executeQuery(sql);
+
+			sql = "CREATE TABLE Group_chats " +
+	  		"(group_name VARCHAR(20) NOT NULL, " +
+	  		" duration INT NOT NULL, " +
+	  		" owner VARCHAR(20) NOT NULL, " +
+	  		" PRIMARY KEY (group_name))";
+			st.executeQuery(sql);
+
+			sql = "CREATE TABLE Private_chats " +
+	  		"(pc_id INT NOT NULL, " +
+	  		" member_1 VARCHAR(20) NOT NULL, " +
+	  		" member_2 VARCHAR(20) NOT NULL, " +
+			" PRIMARY KEY (pc_id))";
+			st.executeQuery(sql);
+
+			sql = "CREATE TABLE Messages " + 
+	  		"(message_id INT NOT NULL, " +
+	  		" text_string VARCHAR(1400) NOT NULL, " +
+	  		" timestamp DATE NOT NULL, " +
+	  		" type VARCHAR(15) NOT NULL, " +
+	  		" is_public VARCHAR(15) NOT NULL, " +
+	  		" sender VARCHAR(20) NOT NULL, " +
+	  		" receiver VARCHAR(20) NOT NULL, " +
+	  		" PRIMARY KEY (message_id))";
+			st.executeQuery(sql);
+
+			sql = "CREATE TABLE Contact_pending_lists " +
+	  		"(receiver VARCHAR(20) NOT NULL, " +
+	  		" sender VARCHAR(20) NOT NULL)";
+			st.executeQuery(sql);
+
+			sql = "CREATE TABLE Contact_lists " +
+	  		"(owner VARCHAR(20) NOT NULL, " +
+	  		" friend VARCHAR(20) NOT NULL)";
+			st.executeQuery(sql);
+
+			sql = "CREATE TABLE Group_pending_lists " +
+	  		"(pending_people VARCHAR(20) NOT NULL, " +
+  			" group_name VARCHAR(20) NOT NULL)";
+			st.executeQuery(sql);
+
+			sql = "CREATE TABLE Group_chat_members " +
+	  		"(group_name VARCHAR(20) NOT NULL, " +
+  			" member VARCHAR(20) NOT NULL)";
+			st.executeQuery(sql);
+			
+			sql = "CREATE TABLE Message_topic_words " +
+	  		"(Topic_word VARCHAR(256) NOT NULL, " +
+	  		" message_id INT NOT NULL)";
+			st.executeQuery(sql);
+
+			sql = "CREATE TABLE User_topic_words " +
+	  		"(Topic_word VARCHAR(256) NOT NULL, " +
+	  		" email_address VARCHAR(20) NOT NULL)";
+			st.executeQuery(sql);
+
+			sql = "CREATE TABLE Group_chat_messages " +
+	  		"(group_name VARCHAR(20) NOT NULL, " +
+	  		" message_id INT NOT NULL)";
+			st.executeQuery(sql);
+
+			sql = "CREATE TABLE Circle_feed_messages " +
+	  		"(circle_id INT NOT NULL, " +
+	  		" message_id INT NOT NULL)";
+			st.executeQuery(sql);
+
+			sql = "CREATE TABLE Private_chat_messages " +
+	  		"(pc_id INT NOT NULL, " +
+	  		" message_id INT NOT NULL)";
+			st.executeQuery(sql);
+
+		}
+		catch(Exception e){System.out.println(e);}
+	}
+
+	public static void addForeignKeys(Connection con){
+		try{
+			// Add foreign keys
+			Statement st = con.createStatement();
+		
+			String sql = "ALTER TABLE Managers " +
+	  		"ADD FOREIGN KEY (email_address) REFERENCES Users(email_address)";
+			st.executeQuery(sql);
+
+			sql = "ALTER TABLE Circle_feeds " +
+	  		"ADD FOREIGN KEY (owner) REFERENCES Users(email_address)";
+			st.executeQuery(sql);
+
+			sql = "ALTER TABLE Group_chats " +
+	  		"ADD FOREIGN KEY (owner) REFERENCES Users(email_address)";
+			st.executeQuery(sql);
+			
+			sql = "ALTER TABLE Private_chats " +
+	  		"ADD FOREIGN KEY (member_1) REFERENCES Users(email_address) " +
+	  		"ADD FOREIGN KEY (member_2) REFERENCES Users(email_address)";
+			st.executeQuery(sql);
+
+			sql = "ALTER TABLE Messages " + 
+	  		"ADD FOREIGN KEY (sender) REFERENCES Users(email_address) " +
+	  		"ADD FOREIGN KEY (receiver) REFERENCES Users(email_address)";
+			st.executeQuery(sql);
+
+			sql = "ALTER TABLE User_topic_words " + 
+	  		"ADD FOREIGN KEY (email_address) REFERENCES Users(email_address)";
+			st.executeQuery(sql);
+
+			sql = "ALTER TABLE Contact_pending_lists " + 
+	  		"ADD FOREIGN KEY (sender) REFERENCES Users(email_address) " +
+	  		"ADD FOREIGN KEY (receiver) REFERENCES Users(email_address)";
+			st.executeQuery(sql);
+
+			sql = "ALTER TABLE Contact_lists " +
+	  		"ADD FOREIGN KEY (owner) REFERENCES Users(email_address) " +
+	  		"ADD FOREIGN KEY (friend) REFERENCES Users(email_address)";
+			st.executeQuery(sql);
+
+			sql = "ALTER TABLE Group_pending_lists " +
+	  		"ADD FOREIGN KEY (pending_people) REFERENCES Users(email_address) " +
+	  		"ADD FOREIGN KEY (group_name) REFERENCES Group_chats(group_name)";
+			st.executeQuery(sql);
+
+			sql = "ALTER TABLE Message_topic_words " +
+	  		"ADD FOREIGN KEY (message_id) REFERENCES Messages(message_id)";
+			st.executeQuery(sql);
+
+			sql = "ALTER TABLE Group_chat_messages " +
+			"ADD FOREIGN KEY (group_name) REFERENCES Group_chats(group_name) " +
+	  		"ADD FOREIGN KEY (message_id) REFERENCES Messages(message_id)";
+			st.executeQuery(sql);
+
+			sql = "ALTER TABLE Circle_feed_messages " +
+			"ADD FOREIGN KEY (circle_id) REFERENCES Circle_feeds(circle_id) " +
+	  		"ADD FOREIGN KEY (message_id) REFERENCES Messages(message_id)";
+			st.executeQuery(sql);
+
+			sql = "ALTER TABLE Group_chat_members " +
+			"ADD FOREIGN KEY (group_name) REFERENCES Group_chats(group_name) " +
+	  		"ADD FOREIGN KEY (member) REFERENCES Users(email_address)";
+			st.executeQuery(sql);
+
+			sql = "ALTER TABLE Private_chat_messages " +
+			"ADD FOREIGN KEY (pc_id) REFERENCES Private_chats(pc_id) " +
+	  		"ADD FOREIGN KEY (message_id) REFERENCES Messages(message_id)";
+			st.executeQuery(sql);
+			
+		}
+		catch(Exception e){System.out.println(e);}
+	}
+
+	public static void deleteTables(Connection con){
+		try{
+			Statement st = con.createStatement();
+			String sql = "DROP TABLE USERS CASCADE CONSTRAINTS";
+			st.executeUpdate(sql);
+			sql = "DROP TABLE MANAGERS CASCADE CONSTRAINTS";
+			st.executeUpdate(sql);
+			sql = "DROP TABLE CIRCLE_FEEDS CASCADE CONSTRAINTS";
+			st.executeUpdate(sql);
+			sql = "DROP TABLE GROUP_CHATS CASCADE CONSTRAINTS";
+			st.executeUpdate(sql);
+			sql = "DROP TABLE PRIVATE_CHATS CASCADE CONSTRAINTS";
+			st.executeUpdate(sql);
+			sql = "DROP TABLE MESSAGES CASCADE CONSTRAINTS";
+			st.executeUpdate(sql);
+			sql = "DROP TABLE CONTACT_PENDING_LISTS CASCADE CONSTRAINTS";
+			st.executeUpdate(sql);
+			sql = "DROP TABLE CONTACT_LISTS CASCADE CONSTRAINTS";
+			st.executeUpdate(sql);
+			sql = "DROP TABLE GROUP_PENDING_LISTS CASCADE CONSTRAINTS";
+			st.executeUpdate(sql);
+			sql = "DROP TABLE GROUP_CHAT_MEMBERS CASCADE CONSTRAINTS";
+			st.executeUpdate(sql);
+			sql = "DROP TABLE Message_topic_Words CASCADE CONSTRAINTS";
+			st.executeUpdate(sql);
+			sql = "DROP TABLE User_topic_words CASCADE CONSTRAINTS";
+			st.executeUpdate(sql);
+			sql = "DROP TABLE Group_chat_messages CASCADE CONSTRAINTS";
+			st.executeUpdate(sql);
+			sql = "DROP TABLE Circle_feed_messages CASCADE CONSTRAINTS";
+			st.executeUpdate(sql);
+			sql = "DROP TABLE Private_chat_messages CASCADE CONSTRAINTS";
+			st.executeUpdate(sql);
+		}
+		catch(Exception e){System.out.println(e);}
+	}
 
 	public static void printTable(Connection con, String TBName){
 		try{
@@ -65,7 +271,6 @@ public class DBManager {
 					}
 				}
 			}	
-			System.out.println();
 			// print primary keys
 			DatabaseMetaData dm = con.getMetaData();
 			rs = dm.getPrimaryKeys(null, null, TBName);
@@ -82,169 +287,26 @@ public class DBManager {
                 catch(Exception e){System.out.println(e);}
 	}
 
-	public static void deleteTables(Connection con){
+	public static void printGLEETables(Connection con){
 		try{
-			Statement st = con.createStatement();
-			String sql = "DROP TABLE USERS CASCADE CONSTRAINTS";
-			st.executeUpdate(sql);
-			sql = "DROP TABLE MANAGERS CASCADE CONSTRAINTS";
-			st.executeUpdate(sql);
-			sql = "DROP TABLE CIRCLE_FEEDS CASCADE CONSTRAINTS";
-			st.executeUpdate(sql);
-			sql = "DROP TABLE GROUP_CHATS CASCADE CONSTRAINTS";
-			st.executeUpdate(sql);
-			sql = "DROP TABLE PRIVATE_CHATS CASCADE CONSTRAINTS";
-			st.executeUpdate(sql);
-			sql = "DROP TABLE MESSAGES CASCADE CONSTRAINTS";
-			st.executeUpdate(sql);
-			sql = "DROP TABLE CONTACT_PENDING_LISTS CASCADE CONSTRAINTS";
-			st.executeUpdate(sql);
-			sql = "DROP TABLE CONTACT_LISTS CASCADE CONSTRAINTS";
-			st.executeUpdate(sql);
-			sql = "DROP TABLE GROUP_PENDING_LISTS CASCADE CONSTRAINTS";
-			st.executeUpdate(sql);
-			sql = "DROP TABLE TOPIC_WORDS CASCADE CONSTRAINTS";
-			st.executeUpdate(sql);
+			DatabaseMetaData md = con.getMetaData();
+			ResultSet rs = md.getTables(null, "GLEE", "%", null);
+			while (rs.next()) {
+  				System.out.println(rs.getString(3));
+			}
 		}
 		catch(Exception e){System.out.println(e);}
 	}
 
-	public static void createTables(Connection con){
+	public static void printAllTables(Connection con){
 		try{
-			// Create tables without foreign keys
-			Statement st = con.createStatement();
-			
-			String sql = "CREATE TABLE Users " +
-	  		"(email_address VARCHAR(20) NOT NULL, " +
-	  		" password VARCHAR(10) NOT NULL, " +
-	  		" name VARCHAR(20) NOT NULL, " +
-	  		" phone_number NUMERIC(10) NOT NULL, " +
-	  		" screenname VARCHAR(20), " +
-			" Topic_words VARCHAR(30), " +
-	  		" Contact_pending_list INT, " +
-			" Contact_list INT, " +
-	  		" PRIMARY KEY (email_address))";
-			st.executeQuery(sql);	
-		
-			sql = "CREATE TABLE Managers " +
-	  		"(email_address VARCHAR(20) NOT NULL, " +
-	  		" PRIMARY KEY (email_address))";
-			st.executeQuery(sql);
-
-			sql = "CREATE TABLE Circle_feeds " +
-	  		"(Circle_id INT NOT NULL, " +
-	  		" message_id INT NOT NULL, " +
-	  		" owner VARCHAR(20) NOT NULL, " +
-	  		" PRIMARY KEY (Circle_id), " +
-	  		" UNIQUE (owner))";
-			st.executeQuery(sql);
-
-			sql = "CREATE TABLE Group_chats " +
-	  		"(name VARCHAR(20) NOT NULL, " +
-	  		" duration INT NOT NULL, " +
-	  		" owner VARCHAR(20) NOT NULL, " +
-	  		" members VARCHAR(20) NOT NULL, " +
-	  		" message_id INT NOT NULL, " +
-	  		" Group_pending_list INT NOT NULL, " +
-	  		" PRIMARY KEY (name))";
-			st.executeQuery(sql);
-
-			sql = "CREATE TABLE Private_chats " +
-	  		"(message_id INT NOT NULL, " +
-	  		" member_1 VARCHAR(20) NOT NULL, " +
-	  		" member_2 VARCHAR(20) NOT NULL)";
-			st.executeQuery(sql);
-
-			sql = "CREATE TABLE Messages " + 
-	  		"(message_id INT NOT NULL, " +
-	  		" text_string VARCHAR(1400) NOT NULL, " +
-	  		" timestamp DATE NOT NULL, " +
-	  		" type VARCHAR(15) NOT NULL, " +
-	  		" is_public VARCHAR(15) NOT NULL, " +
-	  		" sender VARCHAR(20) NOT NULL, " +
-	  		" receiver VARCHAR(20) NOT NULL, " +
-	  		" Topic_words VARCHAR(30) NOT NULL, " +
-	  		" PRIMARY KEY (message_id))";
-			st.executeQuery(sql);
-
-			sql = "CREATE TABLE Contact_Pending_lists " +
-	  		"(cp_id INT NOT NULL, " +
-	  		" pending_people VARCHAR(20) NOT NULL, " +
-	  		" PRIMARY KEY (cp_id))";
-			st.executeQuery(sql);
-
-			sql = "CREATE TABLE Contact_lists " +
-	  		"(c_id INT NOT NULL, " +
-	  		" contacts VARCHAR(20) NOT NULL, " +
-	  		" PRIMARY KEY (c_id))";
-			st.executeQuery(sql);
-
-			sql = "CREATE TABLE Group_pending_lists " +
-	  		"(gp_id INT NOT NULL, " +
-	  		" pending_people VARCHAR(20) NOT NULL, " +
-	  		" PRIMARY KEY (gp_id))";
-			st.executeQuery(sql);
-			
-			sql = "CREATE TABLE Topic_words " +
-	  		"(Topic_word VARCHAR(30) NOT NULL, " +
-	  		" PRIMARY KEY (Topic_word))";
-			st.executeQuery(sql);
+			String[] tbNames = {"USERS", "MANAGERS", "CIRCLE_FEEDS", "GROUP_CHATS", "PRIVATE_CHATS", "MESSAGES", "CONTACT_PENDING_LISTS", "CONTACT_LISTS", "GROUP_PENDING_LISTS", "GROUP_CHAT_MEMBERS", "MESSAGE_TOPIC_WORDS", "USER_TOPIC_WORDS", "GROUP_CHAT_MESSAGES", "CIRCLE_FEED_MESSAGES", "PRIVATE_CHAT_MESSAGES"};
+			for(int i=0; i<15; i++){
+				printTable(con, tbNames[i]);
+				System.out.println();
+			}
 		}
 		catch(Exception e){System.out.println(e);}
 	}
 
-	public static void addForeignKeys(Connection con){
-		try{
-			// Add foreign keys
-			Statement st = con.createStatement();
-			
-			String sql = "ALTER TABLE Users " +
-	  		" ADD FOREIGN KEY (Topic_words) REFERENCES Topic_words(Topic_word)" +
-	  		" ADD FOREIGN KEY (Contact_pending_list) REFERENCES Contact_Pending_lists(cp_id)" +
-			" ADD FOREIGN KEY (Contact_list) REFERENCES Contact_lists(c_id)";
-			st.executeQuery(sql);
-		
-			sql = "ALTER TABLE Managers " +
-	  		"ADD FOREIGN KEY (email_address) REFERENCES Users(email_address)";
-			st.executeQuery(sql);
-
-			sql = "ALTER TABLE Circle_feeds " +
-	  		"ADD FOREIGN KEY (message_id) REFERENCES Messages(message_id) " +
-	  		"ADD FOREIGN KEY (owner) REFERENCES Users(email_address)";
-			st.executeQuery(sql);
-
-			sql = "ALTER TABLE Group_chats " +
-	  		"ADD FOREIGN KEY (owner) REFERENCES Users(email_address) " +
-	  		"ADD FOREIGN KEY (members) REFERENCES Users(email_address) " +
-	  		"ADD FOREIGN KEY (message_id) REFERENCES Messages(message_id) " +
-	  		"ADD FOREIGN KEY (Group_pending_list) REFERENCES Group_pending_lists(gp_id)";
-			st.executeQuery(sql);
-			
-			sql = "ALTER TABLE Private_chats " +
-	  		"ADD FOREIGN KEY (message_id) REFERENCES Messages(message_id) " +
-	  		"ADD FOREIGN KEY (member_1) REFERENCES Users(email_address) " +
-	  		"ADD FOREIGN KEY (member_2) REFERENCES Users(email_address)";
-			st.executeQuery(sql);
-
-			sql = "ALTER TABLE Messages " + 
-	  		"ADD FOREIGN KEY (sender) REFERENCES Users(email_address) " +
-	  		"ADD FOREIGN KEY (receiver) REFERENCES Users(email_address) " +
-	  		"ADD FOREIGN KEY (Topic_words) REFERENCES Topic_words(Topic_word)";
-			st.executeQuery(sql);
-
-			sql = "ALTER TABLE Contact_Pending_lists " +
-	  		"ADD FOREIGN KEY (pending_people) REFERENCES Users(email_address)";
-			st.executeQuery(sql);
-
-			sql = "ALTER TABLE Contact_lists " +
-	  		"ADD FOREIGN KEY (contacts) REFERENCES Users(email_address)";
-			st.executeQuery(sql);
-
-			sql = "ALTER TABLE Group_pending_lists " +
-	  		"ADD FOREIGN KEY (pending_people) REFERENCES Users(email_address)";
-			st.executeQuery(sql);
-			
-		}
-		catch(Exception e){System.out.println(e);}
-	}
 }
