7ba4afe6bf98f7ac2f97ea2f8eae813085a65b72
Yuxiang Wang <ethanyuwang@gmail.com>
2016-11-21 23:20:43 -0800

    Basic UI for ContactsJPanel

--- src/ContactsJPanel.java
+++ src/ContactsJPanel.java
@@ -8,82 +8,74 @@ import java.awt.event.MouseEvent;
 public class ContactsJPanel extends JPanel
 {
     //Java GUI Components
-    JButton loginButton;
-    JButton signUpButton;
     JLabel titleLabel;
-    JTextField emailField;
-    JTextField passField;
+    JTextArea contactTextArea;
+    JTextArea pendingTextArea;
+    JScrollPane contactScroll;
+    JScrollPane pendingScroll;
+    JTextField requestField;
+    JTextField acceptField;
+    JButton requestButton;
+    JButton acceptButton;
+    JButton backButton;
 
     GridBagConstraints gbc;
-    JPanel topPanel;
+    JSplitPane topPanel;
     JPanel botPanel;
 
     public ContactsJPanel()
     {
 	this.repaint();
-	loginButton = new JButton("Login");
-	signUpButton = new JButton("Sign up");
 	titleLabel = new JLabel("CONTACTS");
-	emailField = new JTextField("email");
-	passField = new JTextField("password");
+	contactTextArea = new JTextArea("JFSDJFSDFSJDKFJSD\nFSDFSJDJFSJDFSJklF\nDSKLJFSKLDJFSK\nLDFJSKLDJFSK\nLDJFSKLDFJSKLDJ\nFKLSDJFKLSJKLF");
+	contactTextArea.setEditable(false);
+	contactTextArea.setLineWrap(true);
+	contactTextArea.setWrapStyleWord(false);
+	pendingTextArea = new JTextArea("djklsfjkljfsd PENDING CONTACTS ARE HERE");
+	pendingTextArea.setEditable(false);
+	pendingTextArea.setLineWrap(true);
+	pendingTextArea.setWrapStyleWord(false);
+	contactScroll = new JScrollPane(contactTextArea);
+	pendingScroll = new JScrollPane(pendingTextArea);
+	requestField = new JTextField("enter_email");
+	acceptField = new JTextField("enter_email");
+	requestButton = new JButton("Send Friend Request");
+	acceptButton = new JButton("Accept Friend Request");
+	backButton = new JButton("Back");
 
 	gbc = new GridBagConstraints();
-	topPanel = new JPanel(new BorderLayout());
+	topPanel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, contactScroll, pendingScroll);
 	botPanel = new JPanel(new GridBagLayout());
 
 	//set layout manager for this panel
 	setLayout(new GridLayout(2,1));
 
-	//set title font and size
-	titleLabel.setFont(new Font("Serif", Font.BOLD, 38));
-	titleLabel.setVerticalAlignment(SwingConstants.CENTER);
-	titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
-
 	//add components to top panel
-	topPanel.add(titleLabel, BorderLayout.CENTER);
+	topPanel.setDividerLocation(300);
 
 	//add components to bot panel
 	gbc.gridx = 3;
 	gbc.gridy = 1;
 	gbc.gridwidth = 1;
 	gbc.fill = GridBagConstraints.HORIZONTAL;
-	botPanel.add(emailField, gbc);
+	botPanel.add(requestField, gbc);
 	gbc.gridx = 3;
 	gbc.gridy = 2;
-	botPanel.add(passField, gbc);
+	botPanel.add(requestButton, gbc);
 	gbc.gridx = 3;
 	gbc.gridy = 3;
-	botPanel.add(loginButton, gbc);
+	botPanel.add(acceptField, gbc);
 	gbc.gridx = 3;
 	gbc.gridy = 4;
-	botPanel.add(signUpButton, gbc);
-
-	topPanel.setOpaque(false);
-	botPanel.setOpaque(false);
+	botPanel.add(acceptButton, gbc);
+	gbc.gridx = 3;
+	gbc.gridy = 5;
+	botPanel.add(backButton, gbc);
 
 	//add bot and top panel to this.panel
 	add(topPanel);
 	add(botPanel);
 
 	//event managers
-	signUpButton.addMouseListener(new MouseAdapter() {
-		@Override
-		public void mouseReleased(MouseEvent e) {
-			BuzmoJFrame.setCurrentPanelTo(new SignUpJPanel());
-		}
-	});
-	loginButton.addMouseListener(new MouseAdapter() {
-		@Override
-		public void mouseReleased(MouseEvent e) {
-			Boolean complete = DBInteractor.loginUser(BuzmoJFrame.con, emailField.getText(), passField.getText());
-			if(complete){
-				System.out.println("Login SUCCESS");
-				BuzmoJFrame.setCurrentPanelTo(new NavigationJPanel());
-			}
-			else{
-				System.out.println("Login FAIL");
-			}
-		}
-	});
     }
 }
